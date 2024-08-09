6795eef5bd7a1e07235a7cda5ef8062fa39eab01
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2024-08-09 14:20:47 +0300

    Add controller to display all notes. Add service for note

---
+++ src/main/java/com/manager/passwordmanager/controllers/MainController.java
@@ -0,0 +1,36 @@
+package com.manager.passwordmanager.controllers;
+
+import com.manager.passwordmanager.entity.Note;
+import com.manager.passwordmanager.services.NoteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    private final NoteService noteService;
+
+    public MainController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public String getAllNotes(Model model) {
+        List<Note> notes = noteService.getAllNote();
+        model.addAttribute("notes", notes);
+        model.addAttribute("newNote", new Note());
+        return "index";
+    }
+
+    @PostMapping("/add")
+    public String addNode(@ModelAttribute("newNote") Note note) {
+        noteService.addNewNote(note);
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/manager/passwordmanager/services/NoteService.java
+++ src/main/java/com/manager/passwordmanager/services/NoteService.java
@@ -1,2 +1,40 @@
-package com.manager.passwordmanager.services;public class NoteService {
+package com.manager.passwordmanager.services;
+
+import com.manager.passwordmanager.entity.Note;
+import com.manager.passwordmanager.exceptions.NotFoundException;
+import com.manager.passwordmanager.repositories.NoteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> getAllNote() {
+        return noteRepository.findAll();
+    }
+
+    public Note getNoteById(Long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Note with id = " + id + " not found"));
+    }
+
+    public void addNewNote(Note note) {
+        noteRepository.save(note);
+    }
+
+    public Note getNoteByServiceName(String serviceName) {
+        return null;
+    }
+
+    public void updateNote(Note note) {
+
+    }
+
 }
