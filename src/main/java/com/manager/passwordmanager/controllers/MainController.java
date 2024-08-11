c857f312bb50dad2c69b2b1a81aea3de99921a2d
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2024-08-11 17:41:36 +0300

    Add validation to new note input fields

--- src/main/java/com/manager/passwordmanager/controllers/MainController.java
+++ src/main/java/com/manager/passwordmanager/controllers/MainController.java
@@ -2,9 +2,12 @@ package com.manager.passwordmanager.controllers;
 
 import com.manager.passwordmanager.entity.Note;
 import com.manager.passwordmanager.services.NoteService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,15 +25,27 @@ public class MainController {
     public String getAllNotes(Model model) {
         List<Note> notes = noteService.getAllNote();
         model.addAttribute("notes", notes);
-        model.addAttribute("newNote", new Note());
+
+        if (!model.containsAttribute("newNote")) {
+            model.addAttribute("newNote", new Note());
+        }
         return "index";
     }
 
     @PostMapping("/add")
-    public String addNode(@ModelAttribute("newNote") Note note) {
+    public String addNode(@Valid @ModelAttribute("newNote") Note note, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        // Managing form data and error information for redirects
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.newNote", bindingResult);
+            redirectAttributes.addFlashAttribute("newNote", note);
+            return "redirect:/notes";
+        }
+
         noteService.addNewNote(note);
         return "redirect:/notes";
     }
+
 
     @PostMapping("/delete/{id}")
     public String deleteNote(@PathVariable("id") Long id) {
--- src/main/java/com/manager/passwordmanager/entity/Note.java
+++ src/main/java/com/manager/passwordmanager/entity/Note.java
@@ -1,8 +1,19 @@
 package com.manager.passwordmanager.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -18,15 +29,20 @@ public class Note {
     private Long id;
 
     @Column(name = "service_name")
+    @NotBlank(message = "The field must not be empty")
     private String serviceName;
 
     @Column(name = "service_url")
+    @NotBlank(message = "The field must not be empty")
     private String url;
 
     @Column(name = "service_login")
+    @NotBlank(message = "The field must not be empty")
     private String login;
 
     @Column(name = "hash_password")
+    @NotNull(message = "The field must not be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @Override
